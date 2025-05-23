578b98c549bf2d741741daea2a72c1dff9df31ee
Ciamac.Davoudi <ciamac.davoudil@interone.de>
2025-05-23 15:46:34 +0200

    testing store data

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,27 @@
       <a href="https://pinia.vuejs.org/" target="_blank">
         <img src="./assets/pinia-logo.svg" alt="pinia logo" />
       </a>
-      <h1>Pinit Down</h1>
+      <h1>{{ taskStore.appName }}</h1>
     </header>
+    <div class="task-list-wrapper">
+      <div class="task-list" v-for="task in taskStore.tasks">
+        {{ task.id }}
+        {{ task.title }}
+        {{ task.isFav }}
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
-export default {};
+import { useTaskStore } from "./stores/counter";
+
+export default {
+  setup() {
+    const taskStore = useTaskStore();
+    return { taskStore };
+  },
+};
 </script>
 
 <style scoped></style>
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -2,6 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useTaskStore = defineStore('taskStore', {
   state: () => ({
+    appName: "Pinit Down",
     tasks: [
       {id:1, title:"one", isFav:true},
       {id:2, title:"two", isFav:false},
