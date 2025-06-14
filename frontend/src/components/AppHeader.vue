074a46c891194d0bec5dc8778ae6411a50acdaf0
Cia <53003092+ciamac-da@users.noreply.github.com>
2025-06-14 11:21:41 +0200

    scss architecture done

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -15,58 +15,10 @@
   </template>
   
   <script>
+
   export default {
     props: {
       isDark: Boolean
     }
   }
   </script>
-  
-  <style scoped>
-header {
-  text-align: center;
-  background: blueviolet;
-  padding: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-header img {
-  max-width: 80px;
-  transform: rotate(-10deg);
-  transition: 0.5s linear all;
-  cursor: pointer;
-}
-header img:hover {
-  transform: rotate(6rad);
-}
-
-header h1 {
-  margin: 0;
-  font-size: 48px;
-  padding-top: 25px;
-  margin-left: 15px;
-  color: white;
-  transform: rotate(2deg);
-}
-
-.theme-toggle {
-  position: absolute;
-  right: 12px;
-  top: 85px;
-  cursor: pointer;
-  padding: 8px;
-  background: transparent;
-  border: none;
-}
-
-.theme-toggle:hover {
-  transform: rotate(6rad);
-}
-
-.theme-toggle i {
-  color: #ecb732;
-  font-size: 28px;
-}
-</style>
