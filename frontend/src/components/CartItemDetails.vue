98be10d135cb4c8928afedea9cb8d8ca403585cb
Cia <53003092+ciamac-da@users.noreply.github.com>
2025-06-14 08:44:15 +0200

    moving styling from main file to components scooped

--- frontend/src/components/CartItemDetails.vue
+++ frontend/src/components/CartItemDetails.vue
@@ -27,3 +27,68 @@ export default {
   },
 };
 </script>
+
+<style lang="css" scoped>
+  .cart-item {
+  padding: 6px 20px;
+  background: #fff;
+  margin-top: 20px;
+  border-radius: 4px;
+  box-shadow: 2px 4px 6px rgba(0,0,0,0.05);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.cart-item h3, .cart-item .icons {
+  display: inline-block;
+}
+
+.cart-item .icons {
+  text-align: right;
+}
+
+.cart-item i {
+  font-size: 1.4em;
+  margin-left: 6px;
+  cursor: pointer;
+  color: #bbb;
+  transition: .2s linear all;
+}
+
+.cart-item i:hover {
+  scale: 1.04;
+  transition: .2s linear all;
+}
+
+.cart-item .fav {
+  color: red;
+}
+
+.cart-item i.active,
+.cart-item i:hover
+{
+  color: #ecb732;
+}
+
+.dark .cart-item {
+  background: #333;
+  color: #eee;
+  box-shadow: 2px 4px 6px rgba(255,255,255,0.05);
+}
+
+.dark .cart-item .fav {
+  color: red;
+}
+
+
+.dark .cart-item i {
+  color: #888;
+}
+
+.dark .cart-item i.active,
+.dark .cart-item i:hover {
+  color: #ecb732;
+}
+
+</style>
